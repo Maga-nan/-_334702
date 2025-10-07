e20629709b50709a5e246de5a37322bd9151ea45
334702 Илютко А.И. <160400346+Maga-nan@users.noreply.github.com>
2025-10-07 09:29:49 +0300

    Update MandelbrotSetTask.java

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+           c = c.times(c).times(c).plus(comp); // z^3 + c
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -351,14 +351,15 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(0, 10, 40),
+            Color.BLUE,
+            Color.CYAN,
+            Color.GREENYELLOW,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(0, 255, 128),
+            Color.rgb(0, 40, 10)
         };
+
         
         /**
          * Color stops for colors table: relative position in the table
